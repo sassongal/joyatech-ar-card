52304b08713a13ca65a21c7101cd7774ad239f6d
sassongal <sassong4l@gmail.com>
2026-03-27 14:22:44 +0300

    Add AR experience, business card design, preview page, and config

---
+++ src/targets/joyatech-card-target.js
@@ -0,0 +1,16 @@
+const imageTarget = {
+  "imagePath": "./joyatech-card.jpg",
+  "metadata": {},
+  "name": "joyatech-card",
+  "type": "PLANAR",
+  "properties": {
+    "left": 0,
+    "top": 0,
+    "width": 640,
+    "height": 380,
+    "originalWidth": 1050,
+    "originalHeight": 600,
+    "isRotated": false
+  }
+}
+export default imageTarget;
